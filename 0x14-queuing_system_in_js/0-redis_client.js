cc44085f549349f187e2b925057686a84ebf3ec4
Wiston Venera Macías <wisvem@hotmail.com>
2022-01-09 20:01:06 +0000

    Node redis client

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,7 @@
+import { createClient } from 'redis';
+
+(async () => {
+  const client = createClient();
+  client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+  client.on('ready', () => console.log('Redis client connected to the server'));
+})();
