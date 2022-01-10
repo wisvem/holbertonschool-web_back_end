092bb7defbea3b6e6f08a15bfee4b4e3057817c0
Wiston Venera Macías <wisvem@hotmail.com>
2022-01-10 20:49:45 +0000

    Removed async

--- 0x14-queuing_system_in_js/0-redis_client.js
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -1,7 +1,5 @@
 import { createClient } from 'redis';
 
-(async () => {
-  const client = createClient();
-  client.on('error', (err) => console.log('Redis client not connected to the server:', err));
-  client.on('ready', () => console.log('Redis client connected to the server'));
-})();
+const client = createClient();
+client.on('error', (err) => console.log('Redis client not connected to the server:', err.message));
+client.on('ready', () => console.log('Redis client connected to the server'));
--- 0x14-queuing_system_in_js/1-redis_op.js
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -1,23 +1,21 @@
 import { createClient } from 'redis';
 
-(async () => {
-  const client = createClient();
-  client.on('error', (err) => console.log('Redis client not connected to the server:', err));
-  client.on('ready', () => console.log('Redis client connected to the server'));
-  function setNewSchool(schoolName, value) {
-    client.set(schoolName, value, (err, reply) => {
-      if (err) throw err;
-      console.log(reply);
-    });
-    return (schoolName, value);
-  }
-  function displaySchoolValue(schoolName) {
-    client.get(schoolName, (err, reply) => {
-      if (err) throw err;
-      console.log(reply);
-    });
-  }
-  displaySchoolValue('Holberton');
-  setNewSchool('HolbertonSanFrancisco', '100');
-  displaySchoolValue('HolbertonSanFrancisco');
-})();
+const client = createClient();
+client.on('error', (err) => console.log('Redis client not connected to the server:', err.message));
+client.on('ready', () => console.log('Redis client connected to the server'));
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    if (err) throw err;
+    console.log(reply);
+  });
+  return (schoolName, value);
+}
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) throw err;
+    console.log(reply);
+  });
+}
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
