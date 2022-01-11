fd7f44ff36354b24426e384d1ba1b8a91c2cd8cd
Wiston Venera Macías <wisvem@hotmail.com>
2022-01-11 23:21:09 +0000

    Fixed function to print ok

--- 0x14-queuing_system_in_js/1-redis_op.js
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -4,8 +4,7 @@ const client = createClient();
 client.on('error', (err) => console.log('Redis client not connected to the server:', err));
 client.on('ready', () => console.log('Redis client connected to the server'));
 function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, print, () => {
-  });
+  client.set(schoolName, value, print);
   return (schoolName, value);
 }
 function displaySchoolValue(schoolName) {
