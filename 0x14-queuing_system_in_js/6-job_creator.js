2d6ee5f423ed5e1b75acc96dc6ba748a93cbb450
Wiston Venera Macías <wisvem@hotmail.com>
2022-01-12 00:16:37 +0000

    Job creator and processor

---
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,17 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+const jobFormat = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
+};
+const queueName = 'push_notification_code';
+const job = queue.create(queueName, jobFormat).save((err) => {
+  if (!err) console.log(`Notification job created: ${job.id}`);
+});
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
